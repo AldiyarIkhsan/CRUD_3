7c0a0289514873bc66aaaaa58da8c2f62025f5d6
Ихсанов Алдияр Менжанович <aldiyarikhsanov@gmail.com>
2025-07-19 14:54:52 +0500

    update

--- src/setupTestingRoutes.ts
+++ src/setupTestingRoutes.ts
@@ -3,7 +3,14 @@ import { Express, Request, Response } from "express";
 import { BlogModel } from "./models/BlogModel";
 import { PostModel } from "./models/PostModel";
 
+// В setupTestingRoutes.ts
 export const setupTestingRoutes = (app: Express) => {
+  // 👇 добавь вот это
+  app.delete("testing/all-data", (req, res) => {
+    res.redirect(307, "/testing/all-data"); // сохраняет метод DELETE
+  });
+
+  // 👇 основной рабочий маршрут
   app.delete("/testing/all-data", async (_req, res) => {
     try {
       await Promise.all([BlogModel.deleteMany({}), PostModel.deleteMany({})]);
