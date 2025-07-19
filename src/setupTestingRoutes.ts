9c804500a90e304195a18e9ecf47c339d407779c
Ихсанов Алдияр Менжанович <aldiyarikhsanov@gmail.com>
2025-07-19 15:40:14 +0500

    update

--- src/setupTestingRoutes.ts
+++ src/setupTestingRoutes.ts
@@ -1,23 +1,19 @@
-// In setupTestingRoutes.ts
+// setupTestingRoutes.ts
 import { Express, Request, Response } from "express";
 import { BlogModel } from "./models/BlogModel";
 import { PostModel } from "./models/PostModel";
 
-// В setupTestingRoutes.ts
 export const setupTestingRoutes = (app: Express) => {
-  // 👇 добавь вот это
-  app.delete("testing/all-data", (req, res) => {
-    res.redirect(307, "/testing/all-data"); // сохраняет метод DELETE
-  });
-
-  // 👇 основной рабочий маршрут
-  app.delete("/testing/all-data", async (_req, res) => {
+  app.delete("/testing/all-data", async (_req: Request, res: Response) => {
     try {
-      await Promise.all([BlogModel.deleteMany({}), PostModel.deleteMany({})]);
-      res.sendStatus(204);
+      await Promise.all([
+        BlogModel.deleteMany({}),
+        PostModel.deleteMany({})
+      ]);
+      res.sendStatus(204); // No Content
     } catch (error) {
       console.error("Error clearing data:", error);
-      res.sendStatus(500);
+      res.sendStatus(500); // Internal Server Error
     }
   });
 };
